c10768050052f49c23b17cbbec193298bd7c7363
Ann-Sophie <annso>
2023-04-16 03:09:38 +0200

    credit text

--- credits.js
+++ credits.js
@@ -2,19 +2,20 @@ kaboom();
 
 add([rect(width(), height()), color(98, 160, 83, 0.877)])
 
-// const box = add([
-// 	rect(600, 300),
-// 	pos(350,150),
-// 	color(92,172,238),
-// 	z(2)
-//   ]);
+const box = add([
+	rect(600, 300),
+	pos(350,150),
+	color(90, 150, 82),
+	z(2)
+  ]);
   
-//   const myText = add([
-// 	text("ohhhh, fun little easter egg!"),
-// 	pos(400,280),
-// 	scale(0.28),
-// 	color(255, 255, 255),
-//   ]);
+  const myText = add([
+	text("ohhhh, fun little easter egg!"),
+	pos(400,280),
+	scale(0.28),
+	color(0, 50, 82),
+    z(3),
+  ]);
   
   
-//   myText.text ="Luana Strachan"
+  myText.text ="Luana Strachan\nFanny Heier\nEmilie Huynh\nAnn-Sophie Bullinger"
